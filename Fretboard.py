404bb5168b1eddc6038f208ab3bc71524df6b98a
Syed <syedhoque12345@gmail.com>
2024-07-28 23:57:23 -0400

    updated styling and added chord sounds

--- Fretboard.py
+++ Fretboard.py
@@ -9,12 +9,12 @@ class Fretboard:
         self.play_obj = sa.PlayObject
         self.played = False
         self.fretboard = ["This is the fretboard"]
-        self.fretboard.append(["e_open", 'F_e', 'F#_e', 'G_e', 'G#_e', 'A_e', 'A#_e', 'B_e', 'C_e', 'C#_e', 'D_e', 'D#_e', 'E_12'])
-        self.fretboard.append(['B_open', 'C_B', 'C#_B', 'D_B', 'D#_B', 'E_B', 'F_B', 'F#_B', 'G_B', 'G#_B', 'A_B', 'A#_B', 'B_12'])
-        self.fretboard.append(['G_open', 'G#_G', 'A_G', 'A#_G', 'B_G', 'C_G', 'C#_G', 'D_G', 'D#_G', 'E_G', 'F_G', 'F#_G', 'G_12'])
-        self.fretboard.append(['D_open', 'D#_D', 'E_D', 'F_D', 'F#_D', 'G_D', 'G#_D', 'A_D', 'A#_D', 'B_D', 'C_D', 'C#_D', 'D_12'])
-        self.fretboard.append(['A_open', 'A#_A', 'B_A', 'C_A', 'C#_A', 'D_A', 'D#_A', 'E_A', 'F_A', 'F#_A', 'G_A', 'G#_A', 'A_12'])
-        self.fretboard.append(["E6_open", 'F_E', 'F#_E', 'G_E', 'G#_E', 'A_E', 'A#_E', 'B_E', 'C_E', 'C#_E', 'D_E', 'D#_E', 'E_12'])
+        self.fretboard.append(["e_open", 'F_e', 'F#_e', 'G_e', 'G#_e', 'A_e', 'A#_e', 'B_e', 'C_e', 'C#_e', 'D_e', 'D#_e', '12_e'])
+        self.fretboard.append(['B_open', 'C_B', 'C#_B', 'D_B', 'D#_B', 'E_B', 'F_B', 'F#_B', 'G_B', 'G#_B', 'A_B', 'A#_B', '12_B'])
+        self.fretboard.append(['G_open', 'G#_G', 'A_G', 'A#_G', 'B_G', 'C_G', 'C#_G', 'D_G', 'D#_G', 'E_G', 'F_G', 'F#_G', '12_G'])
+        self.fretboard.append(['D_open', 'D#_D', 'E_D', 'F_D', 'F#_D', 'G_D', 'G#_D', 'A_D', 'A#_D', 'B_D', 'C_D', 'C#_D', '12_D'])
+        self.fretboard.append(['A_open', 'A#_A', 'B_A', 'C_A', 'C#_A', 'D_A', 'D#_A', 'E_A', 'F_A', 'F#_A', 'G_A', 'G#_A', '12_A'])
+        self.fretboard.append(["E6_open", 'F_E', 'F#_E', 'G_E', 'G#_E', 'A_E', 'A#_E', 'B_E', 'C_E', 'C#_E', 'D_E', 'D#_E', '12_E'])
         self.chord_map = CHORD_MAP
         self.scale_map = SCALES_MAP
     
@@ -28,10 +28,10 @@ class Fretboard:
         else: return self.fretboard[string][fret][0]
     
     def get_chord_at(self, chord):
-        return self.chord_map[chord]
-    
-    def get_scale_at(self, scale):
-        return self.scale_map[scale]
+        keys = list(self.chord_map.keys())
+        vals = list(self.chord_map.values())
+        pos = vals.index(chord)
+        return keys[pos]
 
     def get_note_index(self, string, note):
         for j in range(0, 13):
@@ -62,11 +62,8 @@ class Fretboard:
 
     def play_chord(self, notes):
         chord = self.get_chord_at(notes)
-        self.play_obj = self.wav_obj.from_wave_file("Assets/" + chord + ".wav").play()
+        self.play_obj = self.wav_obj.from_wave_file("Assets/" + chord + "_chord" + ".wav").play()
     
-    def play_scale(self, notes):
-        scale = self.get_scale_at(notes)
-        self.play_obj = self.wav_obj.from_wave_file("Assets/" + scale + ".wav").play()
 
     def practice_note(self, targ_note, note):
         if targ_note == note:
--- Screen.py
+++ Screen.py
@@ -2,7 +2,7 @@ import customtkinter as ctk
 from Fretboard import Fretboard
 from Fret_Buttons import FretButton
 from settings import *
-import random
+
 
 
 ctk.set_default_color_theme("classic_theme.json")
@@ -21,12 +21,17 @@ class Screen:
         self.learn_note_label = None
         self.buttons = list()
 
-
         self.screen_width = self.window.winfo_screenwidth()
         self.screen_height = self.window.winfo_screenheight()
         self.window.geometry(f'{self.screen_width}x{self.screen_height}+0+0')
         self.window.minsize(800, 600)
         self.window.maxsize(self.screen_width, self.screen_height)
+
+        self.prog_frame = ctk.CTkFrame(self.window)
+        self.progress = ctk.CTkProgressBar(self.prog_frame, orientation="horizontal", mode="determinate", width=self.screen_width/5, height=50)
+        self.progress.place(x=self.screen_width/2, y=self.screen_height/2, anchor='center')
+        self.prog_frame.place(relwidth=1, relheight=1)
+        self.progress.set(0)
 
         self.main_frame = ctk.CTkFrame(self.window)
         self.jam_frame = ctk.CTkFrame(self.window)
@@ -38,19 +43,30 @@ class Screen:
         self.learn_note_frame = ctk.CTkFrame(self.window)
         self.learn_chord_frame = ctk.CTkFrame(self.window)
         self.learn_scale_frame = ctk.CTkFrame(self.window)
-        
-        self.setup_main_screen()
-        self.setup_jam_screen()
-        self.setup_practice_screen()
-        self.setup_learn_screen()
-        self.setup_practice_note_screen()
-        self.setup_practice_chord_screen()
-        self.setup_practice_scale_screen()
-        self.setup_learn_note_screen()
-        self.setup_learn_chord_screen()
-        self.setup_learn_scale_screen()
-        
-        self.main_screen()
+
+        self.prog_frame.tkraise()
+        self.load_screens()
+
+    def load_screens(self):
+        screens = [
+            self.setup_main_screen,
+            self.setup_jam_screen,
+            self.setup_practice_screen,
+            self.setup_learn_screen,
+            self.setup_practice_note_screen,
+            self.setup_practice_chord_screen,
+            self.setup_practice_scale_screen,
+            self.setup_learn_note_screen,
+            self.setup_learn_chord_screen,
+            self.setup_learn_scale_screen
+        ]
+
+        total_screens = len(screens)
+
+        for i, setup_screen in enumerate(screens):
+            setup_screen()
+            self.progress.set((i + 1) / total_screens)
+        self.prog_frame.place_forget()
 
     # Setup the screens
     def setup_main_screen(self):
@@ -73,16 +89,15 @@ class Screen:
         
         label_frame = ctk.CTkFrame(self.jam_frame, width=self.screen_width)
         label_frame.pack(pady=(20, 50))
-        self.prac_label = ctk.CTkLabel(label_frame, text='Jam!', font=("Oswald", 100))
-        self.prac_label.grid(row=0, column=6, columnspan=2, pady=2, padx=2)
+        label = ctk.CTkLabel(label_frame, text='Jam!', font=("Oswald", 100))
+        label.grid(row=0, column=6, columnspan=2, pady=2, padx=2)
 
         fretboard_frame = ctk.CTkFrame(self.jam_frame)
         fretboard_frame.pack(side="top", pady=(200, 50), ipadx=20, ipady=20)
 
         for data in FRET_POSITIONS.values():
-            FretButton(fretboard_frame, text=data['note'], row=data['row'], col=data['col'], func=lambda: (
-                self.fretboard.play_note(data['row'], data['col'] - 1),
-                self.fretboard.notes_hit.append((data['row'], data['col'] - 1))))
+            button = FretButton(fretboard_frame, text=data["col"] - 1, row=data['row'], col=data['col'], func=None)
+            button.configure(fg_color='#C0C0C0', text_color="black", command=lambda string=data['row'], fret=data['col'] - 1: self.fretboard.play_note(string, fret))
         for data in STRING_POSITIONS.values():
             ctk.CTkLabel(fretboard_frame, text=data['note']).grid(row=data['row'], column=data['col'], padx=2, pady=2)
 
@@ -90,7 +105,7 @@ class Screen:
         back_button_frame.pack()
         submit_button = ctk.CTkButton(back_button_frame, text="Submit", command=lambda: self.fretboard.play_chord(self.fretboard.notes_hit), width=20)
         submit_button.pack(pady=(0, 20))
-        back_button = ctk.CTkButton(back_button_frame, text="Back", command=self.main_screen, width=20)
+        back_button = ctk.CTkButton(back_button_frame, text="Back", command=lambda:(self.main_screen(), self.fretboard.play_obj.stop()), width=20)
         back_button.pack()
 
     def setup_practice_screen(self):
@@ -139,11 +154,11 @@ class Screen:
         dropdown.pack(pady=(0, 20))
 
         fretboard_frame = ctk.CTkFrame(self.prac_note_frame)
-        fretboard_frame.pack()
+        fretboard_frame.pack(side="top", pady=(200, 50), ipadx=20, ipady=20)
     
         for data in FRET_POSITIONS.values():
             button = FretButton(fretboard_frame, text=data["col"] - 1, row=data['row'], col=data['col'], func=None)
-            button.configure(fg_color='#C0C0C0', text_color="black", command=lambda button=button, note=data['note'], string=data['row'], fret=data['col'] - 1: self.prac_note_button_pressed(button, self.user_note, string, fret))
+            button.configure(fg_color='#C0C0C0', text_color="black", command=lambda button=button, string=data['row'], fret=data['col'] - 1: self.prac_note_button_pressed(button, self.user_note, string, fret))
             self.buttons.append(button)
         for data in STRING_POSITIONS.values():
             ctk.CTkLabel(fretboard_frame, text=data['note']).grid(row=data['row'], column=data['col'], padx=2, pady=2)
@@ -162,16 +177,16 @@ class Screen:
         self.prac_chord_label = ctk.CTkLabel(label_frame, text=f'Select a chord:', font=("Oswald", 100))
         self.prac_chord_label.grid(row=0, column=6, columnspan=2, pady=2, padx=2)
 
-        options = ["--select a chord--", 'E', 'Em', 'A', 'Am', 'D', 'Dm', 'G', 'C', 'F']
+        options = ["--select a chord--", 'E', 'Em', 'A', 'Am', 'D', 'Dm', 'G', 'C']
         dropdown = ctk.CTkOptionMenu(self.prac_chord_frame, values=options, command=lambda selected: (self.prac_chord_label.configure(text=f"Practice {selected}"), setattr(self, 'targ_chord', selected)))
         dropdown.pack(pady=(0, 20))
 
         fretboard_frame = ctk.CTkFrame(self.prac_chord_frame)
-        fretboard_frame.pack()
+        fretboard_frame.pack(side="top", pady=(200, 50), ipadx=20, ipady=20)
 
         for data in FRET_POSITIONS.values():
             button = FretButton(fretboard_frame, text=data['note'], row=data['row'], col=data['col'], func=None)
-            button.configure(fg_color='#C0C0C0', text_color="black", command=lambda string=data['row'], fret=data['col'] - 1: self.fretboard.notes_hit.append((string, fret)))
+            button.configure(fg_color='#C0C0C0', text_color="black", command=lambda string=data['row'], fret=data['col'] - 1: (self.fretboard.play_note(string, fret), self.fretboard.notes_hit.append((string, fret))))
         for data in STRING_POSITIONS.values():
             ctk.CTkLabel(fretboard_frame, text=data['note']).grid(row=data['row'], column=data['col'], padx=2, pady=2)
 
@@ -196,11 +211,11 @@ class Screen:
         dropdown.pack(pady=(0, 20))
 
         fretboard_frame = ctk.CTkFrame(self.prac_scale_frame)
-        fretboard_frame.pack()
+        fretboard_frame.pack(side="top", pady=(200, 50), ipadx=20, ipady=20)
 
         for data in FRET_POSITIONS.values():
             button = FretButton(fretboard_frame, text=data['note'], row=data['row'], col=data['col'], func= None)
-            button.configure(fg_color='#C0C0C0', text_color="black", command=lambda string=data['row'], fret=data['col'] - 1: self.fretboard.notes_hit.append((string, fret)))
+            button.configure(fg_color='#C0C0C0', text_color="black", command=lambda string=data['row'], fret=data['col'] - 1:(self.fretboard.play_note(string, fret), self.fretboard.notes_hit.append((string, fret))))
         for data in STRING_POSITIONS.values():
             ctk.CTkLabel(fretboard_frame, text=data['note']).grid(row=data['row'], column=data['col'], padx=2, pady=2)
 
@@ -225,20 +240,17 @@ class Screen:
 
         learning_tabs.add("Why you should learn the notes")
         learning_tabs.add("Patterns")
-        learning_tabs.add("Memorizing Tricks")
 
-        why_label = ctk.CTkLabel(learning_tabs.tab("Why you should learn the notes"), text= "Learning the notes on the fretboard is essential to becoming a better musician. \n"
-                                                                                            "It allows you to understand the music you're playing, and it makes it easier to learn new songs. \n"
-                                                                                            "It also helps you to communicate with other musicians and write your own music.", font=("Arial", 20))
+        why_label = ctk.CTkLabel(learning_tabs.tab("Why you should learn the notes"), text= "Learning the notes on the fretboard is essential to becoming a better musician.\n"
+                                                                                            "It allows you to understand the music you're playing, and it makes it easier to learn new songs.\n"
+                                                                                            "It also helps you to communicate with other musicians and write your own music.")
         why_label.pack(padx=20, pady=20)
-        patterns_label = ctk.CTkLabel(learning_tabs.tab("Patterns"), text="There are many patterns that can help you learn the notes on the fretboard. "
-                                                                          "One of the most common patterns is the octave pattern. "
-                                                                          "The octave pattern is a repeating pattern that starts on the open string and goes up the fretboard.", font=("Arial", 20))
+        patterns_label = ctk.CTkLabel(learning_tabs.tab("Patterns"), text="There are many patterns that can help you learn the notes on the fretboard. \n"
+                                                                          "One pattern is to use the natural notes (A, B, C, D, E, F, G) as a reference point. Starting from A they follow a W-H-W-W-H-W,-\n"
+                                                                          "where W is 2 frets apart and H is 1 fret apart. If you're on the B string and you want to find G, following the pattern you should move 8 frets.\n"
+                                                                          "Another pattern is the 5th fret of the current string is the open string of the string below it. For example, the 5th fret of the low E string A and the 5th fret of A is D"
+                                                                          "For the reverse the 7th fret is where you'll find the open string. For example, the 7th fret of D is A and the 7th fret of A is E")
         patterns_label.pack(padx=20, pady=20)
-        tricks_label = ctk.CTkLabel(learning_tabs.tab("Memorizing Tricks"), text="There are many tricks that can help you memorize the notes on the fretboard. "
-                                                                                "One trick is to use a mnemonic device to help you remember the notes. "
-                                                                                "For example, you could use the phrase", font=("Arial", 20))
-        tricks_label.pack(padx=20, pady=20)
 
         back_button_frame = ctk.CTkFrame(self.learn_note_frame)
         back_button_frame.pack()
@@ -260,14 +272,24 @@ class Screen:
 
         learning_tabs.add("Why you should learn chords")
         learning_tabs.add("Transition Tricks")
+        learning_tabs.add("Learn Chord positions")
 
-        why_label = ctk.CTkLabel(learning_tabs.tab("Why you should learn chords"), text= "Chords are used in many songs. \n"
-                                                                                         "They help you learn the notes on the fretboard.\n"
-                                                                                         "You can improve your finger dexterity and strength", font=("Arial", 20))
+        why_label = ctk.CTkLabel(learning_tabs.tab("Why you should learn chords"), text= "-Chords are used in many songs.\n"
+                                                                                         "-They help you learn the notes on the fretboard.\n"
+                                                                                         "-You can improve your finger dexterity and strength")
         why_label.pack(padx=20, pady=20)
-        tricks_label = ctk.CTkLabel(learning_tabs.tab("Transition Tricks"), text= "There are many tricks that can help you transition between chords. \n"
-                                                                                  "Anticipate the next chord and place your fingers such that you can keep as many finger on the same fret as possible.", font=("Arial", 20))
+        tricks_label = ctk.CTkLabel(learning_tabs.tab("Transition Tricks"), text= "There are many tricks that can help you transition between chords.\n"
+                                                                                  "-Anticipate the next chord and place your fingers such that you can keep as many finger on the same fret as possible.\n"
+                                                                                  "For example, the Am to C transition, you can keep your 1st and 2nd fingers in the same positions and move your third finger to the 3rd fret of the A string.\n"
+                                                                                  "-When you're practicing use a metronome to force yourself to form a different chord in a limited time frame.")
         tricks_label.pack(padx=20, pady=20)
+        positions_dropdown = ctk.CTkOptionMenu(learning_tabs.tab("Learn Chord positions"), values=["--select a chord--", 'E', 'Em', 'A', 'Am', 'D', 'Dm', 'G', 'C', 'F(Partial barre)'], command=lambda selected: self.learn_chord_label.configure(text=f'{CHORD_MAP[selected]}'))
+        positions_dropdown.pack(pady=(20, 20))
+        positions_label = ctk.CTkLabel(learning_tabs.tab("Learn Chord positions"), text="-Select a chord to learn the finger positions. Finger placement are in order from 1st finger to 4th.\n"
+                                                                                        "For example, ((5, 2), (4, 2)) means 1st finger on the 5th string 2nd fret. The 2nd finger on the 4th string 2nd fret.\n"
+                                                                                        "-For barres like F(Barre), your first finger goes on all strings 1st fret and follow the same rules as above.\n"
+                                                                                        "-For partial barres like F(Partial barre), your first finger goes on strings that have the same frets, so 1st and 2nd string. You follow the same rules as above for the rest.")
+        positions_label.pack(padx=20, pady=20)
 
         back_button_frame = ctk.CTkFrame(self.learn_chord_frame)
         back_button_frame.pack()
@@ -287,10 +309,16 @@ class Screen:
         learning_tabs.pack(pady=(20, 20))
 
         learning_tabs.add("Why you should learn scales")
+        learning_tabs.add("Learn Scale positions")
 
-        why_label = ctk.CTkLabel(learning_tabs.tab("Why you should learn scales"), text= "They help you learn the notes on the fretboard.\n"
-                                                                                         "You can improve your finger dexterity and strength", font=("Arial", 20))
+        why_label = ctk.CTkLabel(learning_tabs.tab("Why you should learn scales"), text= "-They help you learn the notes on the fretboard.\n"
+                                                                                         "-You can improve your finger dexterity and strength\n"
+                                                                                         "-Practice the chromatic scale to familiarize yourself with all the notes on the fretboard!")
         why_label.pack(padx=20, pady=20)
+        position_dropdown = ctk.CTkOptionMenu(learning_tabs.tab("Learn Scale positions"), values=["--select a scale--", "E Minor Pentatonic", "A Minor Pentatonic", "C Major"], command=lambda selected: self.learn_scale_label.configure(text=f'{SCALES_MAP[selected]}', font=("Oswald", 30)))
+        position_dropdown.pack(pady=(20, 20))
+        positions_label = ctk.CTkLabel(learning_tabs.tab("Learn Scale positions"), text="Select a scale to learn the finger positions.")
+        positions_label.pack(padx=20, pady=20)
 
         back_button_frame = ctk.CTkFrame(self.learn_scale_frame)
         back_button_frame.pack()
@@ -334,7 +362,6 @@ class Screen:
         self.learn_note_frame.place_forget()
         self.learn_chord_frame.place_forget()
         self.learn_scale_frame.place_forget()
-        self.main_frame.place_forget()
 
         self.practice_frame.place(relwidth=1, relheight=1)
     
@@ -443,23 +470,24 @@ class Screen:
             button.configure(fg_color="red")
             correct_fret = self.fretboard.get_note_index(string, note)[1]
             self.prac_note_label.configure(text=f"Incorrect! It's at fret {correct_fret}")
-    #TODO add chord sound functionality
+
     def prac_chord_button_pressed(self, chord):
         self.fretboard.practice_chord(chord, self.fretboard.notes_hit)
         if self.fretboard.played:
             self.prac_chord_label.configure(text="Correct!")
+            self.fretboard.play_chord(chord)
             self.fretboard.notes_hit.clear()
         else:
-            self.prac_chord_label.configure(text=f"Incorrect! The finger placement(s) are at {chord}", font=("Oswald", 75))
+            self.prac_chord_label.configure(text=f"Incorrect! The finger placement(s) are at {chord}", font=("Oswald", 50))
             self.fretboard.notes_hit.clear()
-    #TODO add scale sound functionality
+
     def prac_scale_button_pressed(self, scale):
         self.fretboard.practice_scale(scale, self.fretboard.notes_hit)
         if self.fretboard.played:
             self.prac_scale_label.configure(text="Correct!")
             self.fretboard.notes_hit.clear()
         else:
-            self.prac_scale_label.configure(text=f"Incorrect! The finger placement(s) are at {scale}", font=("Oswald", 50))
+            self.prac_scale_label.configure(text=f"Incorrect! The finger placement(s) are at {scale}", font=("Oswald", 30))
             self.fretboard.notes_hit.clear()
 
     def reset_button_colors(self):
